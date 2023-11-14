36603549fdcfcb02fb5158a9fc56fe7a7cac0aa0
ciweiss <149791569+ciweiss@users.noreply.github.com>
2023-11-14 17:50:43 +0100

    Update serial_schnittstelle.py
    now sending in 64 byte packages

--- serial_schnittstelle.py
+++ serial_schnittstelle.py
@@ -1,14 +1,22 @@
 import serial
 import struct
 
-ser_mot1 = serial.Serial('COM4', 115200) 
-data="12345.000"
+ser_mot1 = serial.Serial('COM23', 115200,timeout=None) 
 value = 5.14321
-print(value)
+def wrapper(bytearray):
+    package=[]
+    for i in range(int(len(bytearray)/16)):
+        temp=bytearray[i*16:i*16+16]
+        package.append(temp)
+    return package    
+def send_all(package, serial_connection):
+    for i in range(len(package)):
+        serial_connection.write(package[i])
+
+
 ba = bytearray(struct.pack("f", value))
-print(len(ba))
-print(type(ba))
-print(struct.unpack('f', ba))
-ser_mot1.write(ba)
+for i in range(143):
+    ba.extend(struct.pack("f", i))
+send_all(wrapper(ba),ser_mot1)
+print(ser_mot1.read(4))
 ser_mot1.close()
-###https://forum.arduino.cc/t/how-to-convert-byte-array-to-float-array/1030827/5
