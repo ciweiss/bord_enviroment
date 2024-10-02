5f2acc83f4002d622dbf4ae5576dbdceb12d569e
ciweiss <149791569+ciweiss@users.noreply.github.com>
2024-10-02 12:50:19 +0200

    Add files via upload

--- ui2_support.py
+++ ui2_support.py
@@ -16,7 +16,7 @@ import normal_vectors
 import serial
 import ui2
 import struct
-
+import random
 
 _debug = True # False to eliminate debug printing from callback functions.
 
@@ -28,19 +28,93 @@ def main(*args):
     # Creates a toplevel widget.
     global _top1, _w1
     global positions
-    global ser_mot, connected, arm, permutation, was_reseted,scales, coms
-    ser_mot=serial.Serial(None, 115200,timeout=None) 
-    scales=[]
-    scales.append(serial.Serial(None, 115200,timeout=None))
-    scales.append(serial.Serial(None, 115200,timeout=None))
-    scales.append(serial.Serial(None, 115200,timeout=None))
-    connected=False
-    was_reseted=True
-    positions =np.zeros(36)
+    global ser_mot, connected, arm, permutation, was_reseted,scales, coms,entries
     _top1 = root
     _w1 = ui2.Toplevel1(_top1)
     _w1.angle.set(45)
     _w1.com.set(20)
+    ser_mot=serial.Serial(None, 115200,timeout=None) 
+    scales=[]
+    entries=[]
+    scales.append(serial.Serial(None, 115200,timeout=None))
+    scales.append(serial.Serial(None, 115200,timeout=None))
+    scales.append(serial.Serial(None, 115200,timeout=None))
+    entries.append(_w1.s1)
+    entries.append(_w1.s2)
+    entries.append(_w1.s3)
+    entries.append(_w1.s4)
+    entries.append(_w1.s5)
+    entries.append(_w1.s6)
+    entries.append(_w1.s7)
+    entries.append(_w1.s8)
+    entries.append(_w1.s9)
+    entries.append(_w1.s10)
+    entries.append(_w1.s11)
+    entries.append(_w1.s12)
+    entries.append(_w1.s13)
+    entries.append(_w1.s14)
+    entries.append(_w1.s15)
+    entries.append(_w1.s16)
+    entries.append(_w1.s17)
+    entries.append(_w1.s18)
+    entries.append(_w1.s19)
+    entries.append(_w1.s20)
+    entries.append(_w1.s21)
+    entries.append(_w1.s22)
+    entries.append(_w1.s23)
+    entries.append(_w1.s24)
+    entries.append(_w1.s25)
+    entries.append(_w1.s26)
+    entries.append(_w1.s27)
+    entries.append(_w1.s28)
+    entries.append(_w1.s29)
+    entries.append(_w1.s30)
+    entries.append(_w1.s31)
+    entries.append(_w1.s32)
+    entries.append(_w1.s33)
+    entries.append(_w1.s34)
+    entries.append(_w1.s35)
+    entries.append(_w1.s36)
+    entries.append(_w1.Entry4)
+    entries.append(_w1.Entry5)
+    entries.append(_w1.Entry6)
+    entries.append(_w1.Entry7)
+    entries.append(_w1.Entry8)
+    entries.append(_w1.Entry9)
+    entries.append(_w1.Entry10)
+    entries.append(_w1.Entry11)
+    entries.append(_w1.Entry12)
+    entries.append(_w1.Entry13)
+    entries.append(_w1.Entry14)
+    entries.append(_w1.Entry15)
+    entries.append(_w1.Entry16)
+    entries.append(_w1.Entry17)
+    entries.append(_w1.Entry18)
+    entries.append(_w1.Entry19)
+    entries.append(_w1.Entry20)
+    entries.append(_w1.Entry21)
+    entries.append(_w1.Entry22)
+    entries.append(_w1.Entry23)
+    entries.append(_w1.Entry24)
+    entries.append(_w1.Entry25)
+    entries.append(_w1.Entry26)
+    entries.append(_w1.Entry27)
+    entries.append(_w1.Entry28)
+    entries.append(_w1.Entry29)
+    entries.append(_w1.Entry30)
+    entries.append(_w1.Entry31)
+    entries.append(_w1.Entry32)
+    entries.append(_w1.Entry33)
+    entries.append(_w1.Entry34)
+    entries.append(_w1.Entry35)
+    entries.append(_w1.Entry36)
+    entries.append(_w1.Entry37)
+    entries.append(_w1.Entry38)
+    entries.append(_w1.Entry39)
+    connected=False
+    was_reseted=True
+    positions =np.zeros(36)
+
     coms=[]
     coms.append(_w1.com_scale1)
     coms.append(_w1.com_scale2)
@@ -69,7 +143,15 @@ def connect(*args):
     print(connection, "connected")
 def evaluate_scales():
     print("scales evaluated")
-    root.after(2000, evaluate_scales)  
+    stop=False
+    for i in range(36):
+        value=random.random()
+        entries[i].set(value)
+        if value>0.98 :
+            stop=True
+            entries[i+36].configure(bg="red")
+    if not stop:
+        root.after(2000, evaluate_scales)  
 def connect_scales(*args):
     for i in range(3):
         comport=int(coms[i].get())
